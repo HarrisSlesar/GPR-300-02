522171cab5e1c446214fa14c061bce7f4dcd235c
Harris <harris.slesar@mymail.champlain.edu>
2021-04-02 14:29:25 -0400

    class tutorial

--- animal3D SDK/resource/glsl/4x/vs/04-anim/passTangentBasis_morph_transform_vs4x.glsl
+++ animal3D SDK/resource/glsl/4x/vs/04-anim/passTangentBasis_morph_transform_vs4x.glsl
@@ -34,11 +34,21 @@
 //		(hint: results can be stored in local variables named after the 
 //		complete tangent basis attributes provided before any changes)
 
+/*
 layout (location = 0) in vec4 aPosition;
 layout (location = 2) in vec3 aNormal;
 layout (location = 8) in vec4 aTexcoord;
 layout (location = 10) in vec3 aTangent;
 layout (location = 11) in vec3 aBitangent;
+*/
+
+//what is part of a single morph target:
+// -> position, normal, tangent
+// -> 16 available, 16 / 3 = 5 (int)
+
+//What is not part of a single morph target
+// -> texcoord - shared because it's always the same in 2D
+// -> bitangent: normal x tangent
 
 struct sModelMatrixStack
 {
@@ -70,7 +80,13 @@ void main()
 {
 	// DUMMY OUTPUT: directly assign input position to output position
 	//gl_Position = aPosition;
+
+	//results of morphing
+	vec4 aPosition;
+	vec3, aTangent, aBitangent, aNormal;
 	
+	//testing: copy the first morph target only
+
 	sModelMatrixStack t = uModelMatrixStack[uIndex];
 	
 	vTangentBasis_view = t.modelViewMatInverseTranspose * mat4(aTangent, 0.0, aBitangent, 0.0, aNormal, 0.0, vec4(0.0));
